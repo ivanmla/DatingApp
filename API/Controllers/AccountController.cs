e338199d5d5277fe7060c6d24fd7656cc359f1b9
ivanmla <ivan0mlakic@gmail.com>
2020-11-25 08:26:47 +0100

    end of section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -14,6 +14,7 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        
         public AccountController(DataContext context, ITokenService tokenService)
         {
             _tokenService = tokenService;
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -7,6 +7,5 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
     }
 }
